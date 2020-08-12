2b15a74a0e348d6ba827758401b27e2f095889be
shinsuke fukuoka <neko5706@softbank.com>
2020-08-12 17:02:07 +0900

    2章 課題7
    2章 課題7 提出

---
+++ 2-7/index.php
@@ -0,0 +1,39 @@
+<?php
+ //配列
+//   $countries = ["America", "Japan", "China", "Korea"];
+//   $countries[4] = "Germany";//追加
+//   echo $countries[0];
+//   echo '<br>';
+//   echo $countries[1];
+//   echo '<br>';
+//   echo $countries[2];
+//   echo '<br>';
+//   echo $countries[3];
+//   echo '<br>';
+//   echo $countries[4];
+//   echo '<br>';
+//   var_dump($countries);
+//   echo '<br>';
+
+
+ //連想配列
+//   $fruits = ["apple" => "りんご", "orange" => "みかん", "grape" =>  "ぶどう"];
+//   $fruits[peach] = "もも";//追加
+//   echo $fruits["apple"];
+//   echo '<br>';
+//   echo $fruits["orange"];
+//   echo '<br>';
+//   echo $fruits["grape"];
+//   echo '<br>';
+//   echo $fruits["peach"];
+//   echo '<br>';
+
+//   var_dump($fruits);
+
+$color = ["red" => "赤", "blue" => "青", "green" => "緑"];
+$color["yellow"] = "黄色";
+
+var_dump($color);
+
+
+?>
