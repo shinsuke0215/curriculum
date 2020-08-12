3d018932a5116ee89a9ed8b27824d830614755d8
shinsuke fukuoka <neko5706@softbank.com>
2020-08-12 21:56:06 +0900

    2章 課題11
    2章 課題11 提出

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+  $my_name = $_POST['my_name'];
+  $my_email = $_POST['my_email'];
+  $password = $_POST['password'];
+  ?>
+  <p>私の名前は、<?php echo $my_name; ?></p>
+  <p>私のメールアドレスは、<?php echo $my_email; ?></p>
+  <p>私のパスワードは、<?php echo $password; ?></p>
