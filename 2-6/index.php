7907440fb3c083fb5de0811200960ca09c69fe67
shinsuke fukuoka <neko5706@softbank.com>
2020-08-12 15:00:49 +0900

    2章 課題6
    2章 課題6 提出

---
+++ 2-6/index.php
@@ -0,0 +1,38 @@
+<?php
+  //while文
+  // $num = 0;
+  // while ($num < 10) {
+  //   echo $num;
+  //   $num++;
+  // }
+
+  //Do...while文
+  // $num = 0;
+  // do {
+  //   echo $num;
+  //   $num++;
+  // } while ($num < 10);
+
+  //For
+  // for ($i = 0; $i <= 10; $i++) {
+  //   echo $i;
+  // }
+
+  //繰り返しを途中で終了 break
+  // while ($num < 10) {
+  //   echo $num;
+  //   $num++;
+  //   if ($num == 5) {
+  //     break;
+  //   }
+  // }
+
+
+  for ($i = 0; $i <= 100; $i++) {
+    echo $i;
+    echo '<br>';
+  }
+
+
+
+?>
