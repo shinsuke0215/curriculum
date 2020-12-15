8d33e166decf067826d15d8d8fac619bc92754cf
shinsuke fukuoka <neko5706@softbank.com>
2020-12-15 17:10:22 +0900

    js 1章 課題3
    js 1章 課題3 提出

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,100 @@
+// //num1とnum2の計算した結果をresultに入れるためにそれぞれ変数を用意します。
+// let num1 = 10;
+// let num2 = 4;
+// let result;
+// //足し算
+// result = num1 + num2;
+// console.log(result);
+// //引き算
+// result = num1 - num2;
+// console.log(result);
+// //掛け算
+// result = num1 * num2;
+// console.log(result);
+// //割り算
+// result = num1 / num2;
+// console.log(result);
+// //剰余
+// result = num1 % num2;
+// console.log(result);
+
+
+// console.log('Hello' + 'World');
+
+// let lastname = '田中';
+// let firstname = '一郎';
+// let fuluname = lastname + firstname;
+
+// console.log(fuluname);
+
+// let x = 10;
+// x++;
+// console.log(x);
+
+// let y = 10;
+// y--;
+// console.log(y);
+
+// console.log(10 === 10);
+// console.log(10 === 5);
+// console.log(10 !== 5);
+// console.log(10 > 20);
+// console.log(10 <= 10);
+// console.log('10' === 10);
+// console.log('10' == 10);
+
+// let score1 = 90;
+// if(score1 >= 70) {
+//   console.log('合格');
+// }else{
+//   console.log('不合格');
+// }
+
+// let score2 = 60;
+// if(score2 >= 70) {
+//   console.log('合格');
+// }else if (score2 === 0){
+//   console.log('0はまずいです...');
+// }else{
+//   console.log('不合格');
+// }
+
+// let c = 15;
+// if (c >= 10 && c <= 20) {
+//   console.log(c);
+// }
+
+// let a = 10;
+// let b = 15;
+// //偶数の条件式
+// if (a % 2 === 0){
+//   console.log(a);
+// }
+// //奇数の条件式
+// if (b % 2 !== 0){
+//   console.log(b);
+// }
+
+let a = 5
+if(a !== 10) {
+  console.log(a);
+}
+
+let b = 12
+if(b >= 10 && b <= 20 && b % 2 === 0){
+  console.log(b);
+}
+
+let x = 10
+if(x >= 10 && x <= 20){
+  console.log('成功です');
+}else{
+  console.log('失敗です');
+}
+
+let y = 0
+if(y % 2 === 0){
+  console.log('偶数です');
+}else if(y % 2 !== 0){
+  console.log('奇数です');
+}
